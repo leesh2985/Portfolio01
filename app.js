223a909548eb1921fc6ef3a56ef33411bd5466da
Seung Lee <leesh2985@naver.com>
2023-08-17 23:35:33 +0900

    todo만들기

---
+++ app.js
@@ -0,0 +1,17 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault(); // 기본 동작 중지
+  loginForm.classList.add(HIDDEN_CLASSNAME); // loginForm hidden추가
+
+  const username = loginInput.value;
+  greeting.innerText = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  console.log(username);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
