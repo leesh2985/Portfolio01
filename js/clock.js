0ead16b922d9e6004e36eb4c73e53c019b370fc1
Seung Lee <leesh2985@naver.com>
2023-08-21 16:37:55 +0900

    다른 방법 주석 표시

--- js/clock.js
+++ js/clock.js
@@ -6,6 +6,8 @@ function getClock() {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours}:${minutes}:${seconds}`;
+  // clock.innerText = new Date().toLocaleTimeString("en-US", { hour12: false }); // 단순화
+  // clock.innerText = new Date().toLocaleTimeString(); // 오후 12시간버전
 }
 
 getClock();
