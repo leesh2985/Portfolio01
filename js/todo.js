f37d7f641e8311521ac454cabc9cc099a9e24607
Seung Lee <leesh2985@naver.com>
2023-08-22 01:43:21 +0900

    삭제

--- js/todo.js
+++ js/todo.js
@@ -2,14 +2,33 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement; // 지울 타켓
+  // console.log(event.target.parentElement);
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+
+  li.appendChild(span); // li에 span연결
+  li.appendChild(button);
+
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value; // 인풋의 현제 벨류를 새로운 변수에 복사
-
-  console.log(toDoInput.value);
-
   toDoInput.value = ""; // 입력 후 다시 비워주기
-  console.log(newTodo, toDoInput.value);
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
