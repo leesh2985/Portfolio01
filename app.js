f0e8147ae34d0fb9c36ce2246c63d33be1a4af38
Seung Lee <leesh2985@naver.com>
2023-08-21 15:21:48 +0900

    새로고침해도 유지

--- app.js
+++ app.js
@@ -18,13 +18,17 @@ function onLoginSubmit(event) {
   console.log(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 console.log(savedUsername);
 
 if (savedUsername === null) {
-  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  // show the greetings
+  paintGreetings(savedUsername);
 }
