dcdb2a75ac26cef3fd9351573ef9d30a6bbe6427
Seung Lee <leesh2985@naver.com>
2023-08-22 02:04:20 +0900

    배열을 문자로 localStorage저장해줌

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = []; // array로 생성
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos)); // JSON.stringify array가 아닌 string
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement; // 지울 타켓
   // console.log(event.target.parentElement);
@@ -28,7 +34,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value; // 인풋의 현제 벨류를 새로운 변수에 복사
   toDoInput.value = ""; // 입력 후 다시 비워주기
-  paintToDo(newTodo);
+  toDos.push(newTodo); // newToDo를 toDos배열에 push
+  paintToDo(newTodo); // 화면에 보여줌
+  saveToDos(); // localStorage 저장
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
