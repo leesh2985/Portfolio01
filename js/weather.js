a68ecec7ca7f95d507d9a1ccf913f234301744a7
Seung Lee <leesh2985@naver.com>
2023-08-23 19:27:13 +0900

    날씨 보잉게 하기

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "c4ea2c3beb5470bdb0d47f9fca8032e2";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude; // 위도
-  const lng = position.coords.longitude; // 경도
-  console.log("you live in", lat, lng);
+  const lon = position.coords.longitude; // 경도
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  // console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 } // 위치확인
 function onGeoError() {
   console.log("Can't find you. No weather for you.");
