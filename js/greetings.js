46af11fe6693e31e0c8b3223b953e4d46747d099
Seung Lee <leesh2985@naver.com>
2023-08-24 02:24:21 +0900

    시간마다 인사말 달라지게함

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const showToDoForm = document.getElementById("todo-form"); // greeting처럼 동작, 변수 겹침
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -14,11 +15,30 @@ function onLoginSubmit(event) {
   localStorage.setItem(USERNAME_KEY, typedUsername); // 왼쪽 이름, 오른쪽 변수
 
   paintGreetings(typedUsername);
+  paintToDoList();
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+
+  // 시간 인사
+  const date = new Date();
+  const hours = date.getHours();
+  let timeGreeting = "Hello";
+
+  if (hours < 12) {
+    timeGreeting = "Good morning";
+  } else if (hours > 12 && hours < 17) {
+    timeGreeting = "Good afternoon";
+  } else {
+    timeGreeting = "Good night";
+  }
+  greeting.innerText = `${timeGreeting} ${username}`;
+}
+
+function paintToDoList() {
+  // paintGreetings비슷하게 동작
+  showToDoForm.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -29,4 +49,5 @@ if (savedUsername === null) {
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
+  paintToDoList();
 }
